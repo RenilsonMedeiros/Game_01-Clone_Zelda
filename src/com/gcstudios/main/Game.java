7fb07a7336cd9e45416277d1e978e8d1d4867850
RenilsonMedeiros <renilsonsilva@aluno.uema.br>
2020-04-27 03:31:48 -0300

    Save Commit
    - Ações do teclado: ok
    - Renderização da tela: ok
    - Jogador aparecendo na tela: ok

--- src/com/gcstudios/main/Game.java
+++ src/com/gcstudios/main/Game.java
@@ -4,12 +4,20 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable {
+import com.gcstudios.entities.Entity;
+import com.gcstudios.entities.Player;
+import com.gcstudios.graficos.Spritesheet;
+
+public class Game extends Canvas implements Runnable, KeyListener {
 
 	private static final long serialVersionUID = 1L;
 	public static JFrame frame;
@@ -20,11 +28,20 @@ public class Game extends Canvas implements Runnable {
 	private final int SCALE = 3;
 
 	private BufferedImage image;
+	
+	public List<Entity> entities;
+	public Spritesheet spritesheet;
 
 	public Game() {
+		addKeyListener(this);
 		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
 		initFrame();
+		//Inicializando objetos.
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+		entities = new ArrayList<Entity>();
+		spritesheet = new Spritesheet("/spritesheet.png");
+		
+		entities.add(new Player(0, 0, 16, 16, spritesheet.getSprite(32, 0, 16, 16)));
 	}
 
 	private void initFrame() {
@@ -58,7 +75,10 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void tick() {
-		
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.tick();
+		}
 	}
 	
 	public void render() {
@@ -68,11 +88,15 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 		Graphics g = image.getGraphics();
-		g.setColor(new Color(0,0,0));
+		g.setColor(new Color(0,255,0));
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
 		/* Renderização do jogo */
 		//Graphics2D g2 = (Graphics2D) g;
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
 		
 		g.dispose();
 		g = bs.getDrawGraphics();
@@ -109,5 +133,51 @@ public class Game extends Canvas implements Runnable {
 		}
 		
 		stop();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if
+		(
+			e.getKeyCode() == KeyEvent.VK_RIGHT ||
+			e.getKeyCode() == KeyEvent.VK_D
+		) {
+			// Execute bloco 1 - direita
+			System.out.println("Direita");
+		} else if
+			(
+				e.getKeyCode() == KeyEvent.VK_LEFT ||
+				e.getKeyCode() == KeyEvent.VK_A
+			) {
+				// Execute bloco 2 - esquerda
+				System.out.println("Esquerda");
+			}
+		
+		if
+		(
+			e.getKeyCode() == KeyEvent.VK_UP ||
+			e.getKeyCode() == KeyEvent.VK_W
+		) {
+			System.out.println("Cima");
+		} else if
+			(
+				e.getKeyCode() == KeyEvent.VK_DOWN ||
+				e.getKeyCode() == KeyEvent.VK_S
+			) {
+				// Execute bloco 2 - baixo
+				System.out.println("Baixo");
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
