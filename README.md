17ee4c6287dbd6c7acc145b0c0e950b0297d4b35
RenilsonMedeiros <renilsonsilva@aluno.uema.br>
2020-04-26 23:07:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Game_01-Clone_Zelda
+ Curso Desenvolvimento de Games disponibilizados pela Danki.code
---
+++ src/com/gcstudios/main/Game.java
@@ -0,0 +1,113 @@
+package com.gcstudios.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+	public static JFrame frame;
+	private Thread thread;
+	private Boolean isRunning = true;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 3;
+
+	private BufferedImage image;
+
+	public Game() {
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+	}
+
+	private void initFrame() {
+		frame = new JFrame("Game #1");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);		
+	}
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	                                         
+	public synchronized void stop() {        
+		isRunning = false;                   
+		try {                                
+			thread.join();                   
+		} catch (InterruptedException e) {   
+			e.printStackTrace();             
+		}                                    
+	}                                        
+	                                         
+	public static void main(String args[]) { 
+	     Game game = new Game();
+	     game.start();
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(0,0,255));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		
+		/* Renderização do jogo */
+		//Graphics2D g2 = (Graphics2D) g;
+		
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
+	}
+
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		
+		while(isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer+= 1000;
+			}
+		}
+		
+		stop();
+	}
+}
